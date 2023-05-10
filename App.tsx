a86a9d6d1046d4972f7104b3dc21d79aa3856304
somasekhar kakarla <somasekharkakarla@gmail.com>
2023-05-10 11:48:25 +0400

    null is supporting in expresssions

--- App.tsx
+++ App.tsx
@@ -56,6 +56,16 @@ function App(): JSX.Element {
 
  let mapComponentView:any=null
 
+
+ const featureTypeIDFilter = [
+      'to-boolean',
+      [
+        'match',
+        ['get', 'featureType'],
+        ...[null, 1, 0],   // replace null with asdfadsf to work
+      ],
+    ];
+
  const onMapPress = (layers:any) => {
   // noinspection JSUnresolvedVariable
   const boundingScreenBox = getBoundingBox([[layers.properties.screenPointX - 10,
@@ -146,7 +156,64 @@ function App(): JSX.Element {
                         "num_modules_horizontal": "1"
                     },
                     "name": "B01 S01 R029 N",
-                    "featureType": "EAF3oKgiNZ1L",
+                    "featureType": null,
+                    "dataProperties": null,
+                    "description": null,
+                    "workflow": null,
+                    "hierarchyProperties": null,
+                    "uid": "gvWcDiVrMYOE",
+                    "workflowProgress": {}
+                }
+            },
+            {
+                "type": "Feature",
+                "geometry": {
+                    "type": "MultiPolygon",
+                    "coordinates": [
+                        [
+                            [
+                                [
+                                    -76.57977345687516,
+                                    36.69512793531269
+                                ],
+                                [
+                                    -76.57979889504533,
+                                    36.69512836586616
+                                ],
+                                [
+                                    -76.57979560082207,
+                                    36.69525875203225
+                                ],
+                                [
+                                    -76.57977016225729,
+                                    36.69525833536608
+                                ],
+                                [
+                                    -76.57977345687516,
+                                    36.69512793531269
+                                ]
+                            ]
+                        ]
+                    ]
+                },
+                "properties": {
+                    "group": "T0jwSxz0jH",
+                    "featureTypeId": 456,
+                    "project": "jtBi3Wh64k",
+                    "extraProperties": {
+                        "Block": "B01",
+                        "project": "",
+                        "col_start": "1",
+                        "row_start": "1",
+                        "orientation": "NS",
+                        "tracker_type": "Short",
+                        "tracker_section": "1",
+                        "total_num_modules": "13",
+                        "num_modules_vertical": "13",
+                        "num_modules_horizontal": "1"
+                    },
+                    "name": "B01 S01 R030 N",
+                    "featureType": "asdfadsf",
                     "dataProperties": null,
                     "description": null,
                     "workflow": null,
@@ -157,14 +224,14 @@ function App(): JSX.Element {
             }]}}>
         <Mapbox.FillLayer
           id="fillLineString"
-          filter={['all', ['==', ['geometry-type'], 'LineString'],
+          filter={['all', featureTypeIDFilter,
           ]}
           style={featureLayerStyles.fillStatusStyles}
         />
         <Mapbox.FillLayer
           id="fillNoStatusLineString"
           filter={['all',
-            ['==', ['geometry-type'], 'LineString'],
+            featureTypeIDFilter,
             // ['has', 'featureTypeId'],
           ]
           }
@@ -172,14 +239,14 @@ function App(): JSX.Element {
         />
         <Mapbox.FillLayer
           id="fillPolygon"
-          filter={['all', ['==', ['geometry-type'], 'Polygon']
+          filter={['all', featureTypeIDFilter
           ]}
           style={featureLayerStyles.fillStatusStyles}
         />
         <Mapbox.FillLayer
           id="fillNoStatusPolygon"
           filter={['all',
-            ['==', ['geometry-type'], 'Polygon']
+           featureTypeIDFilter
           ]
           }
           style={featureLayerStyles.fillEmptyStyles}
@@ -188,13 +255,13 @@ function App(): JSX.Element {
         {/* THERM SPECIFIC */}
         <Mapbox.FillLayer
           id="fillThermalPolygon"
-          filter={['all', ['==', ['geometry-type'], 'Polygon']]
+          filter={['all',featureTypeIDFilter]
           }
           style={featureLayerStyles.fillPolygonStyles}
         />
         <Mapbox.FillLayer
           id="fillThermalNoStatusPolygon"
-          filter={['all',['==', ['geometry-type'], 'Polygon']]
+          filter={['all',featureTypeIDFilter]
           }
           style={featureLayerStyles.fillPolygonEmptyStyles}
         />
